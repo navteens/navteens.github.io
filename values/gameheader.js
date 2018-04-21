67126347d30951d178bcf3dc748c772ec0b6c5e7
julwrites <jump4jupiter@gmail.com>
2018-04-21 18:32:44 +0800

    Limiting number of cards

--- values/gameheader.js
+++ values/gameheader.js
@@ -1,43 +1,39 @@
 
 var empty = 'back.gif';
-var tile = ['img0.gif', 'img1.gif', 'img2.gif', 'img3.gif', 'img4.gif', 'img5.gif',
+var cards = ['img0.gif', 'img1.gif', 'img2.gif', 'img3.gif', 'img4.gif', 'img5.gif',
     'img6.gif', 'img7.gif', 'img8.gif', 'img9.gif', 'img10.gif', 'img11.gif',
     'img12.gif', 'img13.gif', 'img14.gif'];
 
 function htmlcard(name) {
-    return '<img src="' + name + '" alt="tile" \/>';
+    return '<img src="' + name + '" alt="cards" \/>';
 }
 
-function makecard(i) {
-    document.getElementById('t' + i).innerHTML = '<div onclick="return select(' + i + ');">' + htmlcard(empty) + '<\/div>';
+function makecard(name) {
+    document.getElementById('card' + name).innerHTML = '<div onclick="select(' + name + ');">' + htmlcard(empty) + '<\/div>';
 }
 
 var img = [];
 for (var i = 0; i < 15; i++) {
     img[i] = new Image();
-    img[i].src = tile[i];
-    tile[i] = htmlcard(tile[i]);
+    img[i].src = cards[i];
+    cards[i] = htmlcard(cards[i]);
 }
-
-var choices = [];
-var tmr, tno, tid, cid, cnt;
 
 // Specifying the function to call when loading
 window.onload = load;
 
 function load() {
-    for (var i = 0; i <= 29; i++)
+    for (var i = 0; i < cards.length; i++)
         makecard(i);
 
-    tile.sort((a, b) => {
+    cards.sort((a, b) => {
         return (Math.round(Math.random()) - 0.5);
     });
 }
 
+var selected = [];
 function select(sel) {
-    document.getElementById('t' + sel).innerHTML = tile[sel];
+    document.getElementById('card' + sel).innerHTML = cards[sel];
 
-    choices.push(sel);
-
-    return false;
+    selected.push(sel);
 }
