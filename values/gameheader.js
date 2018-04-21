7e31334e6752750746b2a25f444a221906dc31d0
julwrites <jump4jupiter@gmail.com>
2018-04-21 18:09:44 +0800

    changing some names and consolidating functions

--- values/gameheader.js
+++ values/gameheader.js
@@ -4,50 +4,40 @@ var tile = ['img0.gif', 'img1.gif', 'img2.gif', 'img3.gif', 'img4.gif', 'img5.gi
     'img6.gif', 'img7.gif', 'img8.gif', 'img9.gif', 'img10.gif', 'img11.gif',
     'img12.gif', 'img13.gif', 'img14.gif'];
 
-function randOrd(a, b) {
-    return (Math.round(Math.random()) - 0.5);
-}
-
-function card(name) {
+function htmlcard(name) {
     return '<img src="' + name + '" alt="tile" \/>';
 }
 
-function displaycard(i) {
-    document.getElementById('t' + i).innerHTML = '<div onclick="select(' + i + ');return false;">' + card(empty) + '<\/div>';
+function makecard(i) {
+    document.getElementById('t' + i).innerHTML = '<div onclick="return select(' + i + ');">' + htmlcard(empty) + '<\/div>';
 }
 
 var img = [];
 for (var i = 0; i < 15; i++) {
     img[i] = new Image();
     img[i].src = tile[i];
-    tile[i] = card(tile[i]);
+    tile[i] = htmlcard(tile[i]);
 }
 
 var choices = [];
 var tmr, tno, tid, cid, cnt;
 
 // Specifying the function to call when loading
-window.onload = start;
+window.onload = load;
 
-function start() {
+function load() {
     for (var i = 0; i <= 29; i++)
-        displaycard(i);
-    // clearInterval(tid);
-    tmr = cnt = 0;
-    tile.sort(randOrd);
-    cntr();
-    // tid = setInterval('cntr()', 1000);
-}
+        makecard(i);
 
-// function cntr() {
-//     var min = math.floor(tmr / 60);
-//     var sec = tmr % 60;
-//     document.getelementbyid('cnt').value = min + ':' + (sec < 10 ? '0' : '') + sec;
-//     tmr++;
-// }
+    tile.sort((a, b) => {
+        return (Math.round(Math.random()) - 0.5);
+    });
+}
 
 function select(sel) {
     document.getElementById('t' + sel).innerHTML = tile[sel];
 
     choices.push(sel);
+
+    return false;
 }
