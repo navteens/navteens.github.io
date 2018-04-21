2fcf0df43df5929e11185a8c26fba420df2a5035
julwrites <jump4jupiter@gmail.com>
2018-04-21 15:35:10 +0800

    Rewriting body

--- values/gamebody.js
+++ values/gamebody.js
@@ -2,13 +2,19 @@
 // copyright Stephen Chapman, 28th February 2006, 24th December 2009
 // you may copy this script provided that you retain the copyright notice
 
-document.write('<div align="center"><table cellpadding="0" cellspacing="0" border = "0" > ');
+document.write('<div align="center">');
+
+document.write('<table cellpadding="0" cellspacing="0" border ="0">');
 
 for (var a = 0; a <= 5; a++) {
-    document.write(' < tr > ');
+    document.write('<tr>');
     for (var b = 0; b <= 4; b++) {
-        document.write('<td align="center" class="blk" id = "t' + ((5 * a) + b) + '" ></td > ');
-    } document.write(' <\/tr>');
+        document.write('<td align="center" class="blk" id = "t' + ((5 * a) + b) + '"></td>');
+    } document.write('<\/tr>');
 }
 
-document.write('<\/table> < form name = "mem" > <input type="button" id="cnt" value="" onclick="window.start()" \ /> <\ /form ><\/div>');
+document.write('<\/table>');
+
+document.write('<form name = "foot"> <input type="button" id="next" value="Next" onclick="window.start()"\/> <\/form>');
+
+document.write('<\/div>');
